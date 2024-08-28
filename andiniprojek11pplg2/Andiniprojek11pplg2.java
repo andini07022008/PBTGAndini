03b4732aa15bed3d56ec9e156fa51a222f75ba25
andini07022008 <muhamadusep331@gmail.com>
2024-08-28 07:43:06 +0700

    Add files via upload
    variabel dengan 2 jenis data

---
+++ andiniprojek11pplg2/Andiniprojek11pplg2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.andiniprojek11pplg2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Andiniprojek11pplg2 {
+
+   public static void main (String[] args){
+       int a=40;
+       float b=a;
+       System.out.println(a);
+       System.out.println(b);
+   }}
