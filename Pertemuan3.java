d8aeca2e450241433a065214101afbafb408df55
andini07022008 <muhamadusep331@gmail.com>
2024-08-28 09:44:11 +0700

    Add files via upload
    pertemuan3 soal 1

---
+++ Pertemuan3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan3 {
+
+    public static void main(String[] args) {
+        
+        int number= 100;
+        float Var2= 8.99f;
+        double Var3='A';
+        boolean Var4=false;
+        String  Var5="Hallo Dunia";
+    }
+}
