9a5e454c769ec561bf4bf94140cc51865c6bebcc
andini07022008 <muhamadusep331@gmail.com>
2024-08-28 11:21:11 +0700

    Add files via upload
    pertemuan3 soal 2

---
+++ Pertemuan3soal2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan3soal2 {
+
+    /**
+   * @param args the command line arguments 
+     */
+    public static void main(String[] args) {
+        // TODO Code application loginc here
+        int NIM = 2110117000;
+        String NamaMahasiswa = "Andika Jaya Utma";
+        String kelas ="01SIFE020";
+       double Usia = 19.5;
+       double Tinggi = 1.72;
+       
+                System.out.println("NIM: " + NIM);
+                System.out.println("NamaMahasiswa: " + NamaMahasiswa);
+                System.out.println("Kelas : " + kelas);
+                System.out.println("tinggi : " + Tinggi);
+
+    }
+    
+
+}
