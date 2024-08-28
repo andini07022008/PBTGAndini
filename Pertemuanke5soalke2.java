7f6afc7c2bf97772b0b080bd90bc3c7da91e3044
andini07022008 <muhamadusep331@gmail.com>
2024-08-28 13:28:13 +0700

    Add files via upload
    pertemuankelima soal ke 2

---
+++ Pertemuanke5soalke2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuanke5soalke2;
+
+/**
+ *
+ * @author ADNI
+ */
+public class Pertemuanke5soalke2 {
+
+    public static void main(String[] args) {
+        
+        double alas=17;double tinggi=11;
+        double luas= (alas*tinggi)/2;
+        
+           System.out.println("NilaiAlasdanTinggi:" + luas);
+    }
+}
