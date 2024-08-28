97954e6df801be42ad40cd829b2dd6de80551c8a
andini07022008 <muhamadusep331@gmail.com>
2024-08-28 11:36:53 +0700

    Add files via upload
    pertemuan4soal1

---
+++ Pertemuan4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan4;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Pertemuan4 {
+
+    /**
+     * @param args the Command line arguments 
+     */
+    public static void main(String[] args) {
+        //TODO code application logic here
+        int angka1 = 10;
+        int angka2 = 3;
+        
+      System.out.println("A + B: " + (angka1 + angka2) );
+      System.out.println("A - B: " + (angka1 + angka2) );
+      System.out.println("A * b: " + (angka1 * angka2) );
+      System.out.println("A / B: " + (angka1 / angka2) );
+             
+    }
+    
+}
+
