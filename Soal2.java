5eb504313a985cb119cfc670cebdc497b38ec8f1
andini07022008 <muhamadusep331@gmail.com>
2024-08-28 08:08:28 +0700

    Add files via upload
    soalnomer2 contoh program variabel static

---
+++ Soal2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Soal2 {
+
+  public static String MakanandanMinumanFavorit;
+  
+  
+  
+  public static void makanandanminuman(){
+      
+      String MakananFavorit ="mie";
+              String MinumanFavorit ="pop ice";
+              
+              
+              System.out.println("makanan Favorit saya"+"mie");
+              //memanggil variabel makanan favorite saya
+              System.out.println("minuman Favorit saya"+":pop ice")
+                      ;
+              //memanggil variabrl minuman favorit saya 
+           
+  }
+  
+  
+  
+  public static void main(String[] args){
+      
+      Soal2 latihan =new Soal2();
+      
+      Soal2.makanandanminuman();
+  }
+  }
+
+
